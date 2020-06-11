0ce59e641f3b91d8313bca655c0e7039a8a4e7fb
Dániel Szűcs <h880775@stud.u-szeged.hu>
2020-06-11 16:19:42 +0200

    Changed Player default name
    Signed-off-by: Dániel Szűcs <h880775@stud.u-szeged.hu>

--- quasar6/main/Player.py
+++ quasar6/main/Player.py
@@ -3,7 +3,7 @@ class Player:
     Basic class for player objects.
     The program does not handle more then 2 players!
     """
-    def_name_1 = "Player 1"
+    def_name_1 = "Player"
     def_name_2 = "AI"
 
     def __init__(self, name):
